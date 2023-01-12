9813004460e0fa613ab6d4892ae2f036c66d0f71
Kenneth Jones <kennethjones1991@outlook.com>
2023-01-12 09:44:00 -0700

    Change stat cases to match PokeAPI keys

--- Gen3/TempPokemon.swift
+++ Gen3/TempPokemon.swift
@@ -87,11 +87,11 @@ struct TempPokemon: Codable{
                 hp = try statsDictionaryContainer.decode(Int.self, forKey: .value)
             case "attack":
                 attack =  try statsDictionaryContainer.decode(Int.self, forKey: .value)
-            case "defence":
+            case "defense":
                 defence = try statsDictionaryContainer.decode(Int.self, forKey: .value)
-            case "specialAttack":
+            case "special-attack":
                 specialAttack = try statsDictionaryContainer.decode(Int.self, forKey: .value)
-            case "specialDefence":
+            case "special-defense":
                 specialDefence = try statsDictionaryContainer.decode(Int.self, forKey: .value)
             case "speed":
                 speed = try statsDictionaryContainer.decode(Int.self, forKey: .value)

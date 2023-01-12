b2c391b055373908407d225e59a7562f10d7bf0c
Elisei Bobocea <elisey93@icloud.com>
2023-01-12 11:49:16 +0000

    favorite functionality added

--- Gen3/ContentView.swift
+++ Gen3/ContentView.swift
@@ -9,22 +9,30 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    
     
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
         animation: .default)
     private var pokedex: FetchedResults<Pokemon>
     
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
+        predicate:  NSPredicate(format: "favorite = %d", true),
+        animation: .default
+    ) private var favorites: FetchedResults<Pokemon>
+    
+    
+    @State private var filterByFavorites = false
     @StateObject private var pokemonVM = PokemonViewModel(controller: FetchController())
     
     var body: some View {
         
         
-        switch pokemonVM.status {
-        case .success:
+//        switch pokemonVM.status {
+//        case .success:
             NavigationStack {
-                List(pokedex) { pokemon in
+                List(filterByFavorites ? favorites : pokedex) { pokemon in
                     NavigationLink(value: pokemon) {
                         AsyncImage(url: pokemon.sprite) { image in
                             image
@@ -35,6 +43,11 @@ struct ContentView: View {
                         }
                         .frame(width: 100, height: 100)
                         Text(pokemon.name!.capitalized)
+                        
+                        if pokemon.favorite {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.yellow)
+                        }
                     }
                 }
                 .navigationTitle("Pokemon Dex")
@@ -45,13 +58,21 @@ struct ContentView: View {
                 })
                 .toolbar {
                     ToolbarItem(placement: .navigationBarTrailing) {
-                        EditButton()
+                        Button{
+                            withAnimation{
+                                filterByFavorites.toggle()
+                            }
+                        } label: {
+                            Label("Filter By Favorites", systemImage: filterByFavorites ? "star.fill" : "star")
+                        }
+                        .font(.title2)
+                        .foregroundColor(.yellow)
                     }
                 }
             }
-        default:
-            ProgressView()
-        }
+//        default:
+//            ProgressView()
+//        }
     }
 }
 
--- Gen3/PokemonDetail.swift
+++ Gen3/PokemonDetail.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import CoreData
 
 struct PokemonDetail: View {
+    @Environment(\.managedObjectContext) private var viewContext
     @EnvironmentObject var pokemon: Pokemon
     @State var showShiny = false
     
@@ -42,6 +43,27 @@ struct PokemonDetail: View {
                         .cornerRadius(50)
                 }
                 Spacer()
+                Button{
+                    withAnimation {
+                        pokemon.favorite.toggle()
+                        
+                        do {
+                            try viewContext.save()
+                        } catch {
+                         
+                            let nsError = error as NSError
+                            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                        }
+                    }
+                } label: {
+                    if pokemon.favorite {
+                        Image(systemName: "star.fill")
+                    } else {
+                        Image(systemName: "star")
+                    }
+                }
+                .font(.largeTitle)
+                .foregroundColor(.yellow)
             }
             .padding()
             
